b4cffda96626b1edd57f699320dd1c738ced88ac
Adentent <96329894+Adentent@users.noreply.github.com>
2022-01-29 11:58:08 +0800

    Add files via upload

--- MyUniversity.py
+++ MyUniversity.py
@@ -4,40 +4,44 @@ DESIGN: Adentent
 
 from tkinter import *
 
-def quit():
-    exit()
 
-def beginGame():
-    pass
+class Window:
+    class StartWindow:
+        def quit(self):
+            exit()
 
-def main():
-    root = Tk()
-    
-    # init
-    root.title("My University")
+        def beginGame(self):
+            pass
 
-    # main
-    NewGame = Button(root,
-                     text="开始游戏",
-                     font=("zpix", 20),
-                     bg="darkkhaki",
-                     fg="white",
-                     activebackground="darkkhaki",
-                     activeforeground="white",
-                     command=beginGame)
-    NewGame.pack()
-    QuitGame = Button(root,
-                      text="关闭游戏",
-                      font=("zpix", 10),
-                      bg="grey",
-                      fg="white",
-                      activebackground="red",
-                      activeforeground="white",
-                      command=quit)
-    QuitGame.pack()
+        def main(self):
+            root = Tk()
+            
+            # init
+            root.title("My University")
 
-    root.mainloop()
+            # main
+            NewGame = Button(root,
+                            text="开始游戏",
+                            font=("zpix", 20),
+                            bg="darkkhaki",
+                            fg="white",
+                            activebackground="darkkhaki",
+                            activeforeground="white",
+                            command=self.beginGame)
+            NewGame.pack()
+            QuitGame = Button(root,
+                            text="关闭游戏",
+                            font=("zpix", 10),
+                            bg="grey",
+                            fg="white",
+                            activebackground="red",
+                            activeforeground="white",
+                            command=self.quit)
+            QuitGame.pack()
+
+            root.mainloop()
 
 
 if __name__ == "__main__":
-    main()
+    StartWindow = Window().StartWindow()
+    StartWindow.main()
